034aede01fb6bd9ec1a8d78dca1dec22dae4ca88
Leoputera2407 <cookieavarice@gmail.com>
2023-06-02 22:09:23 -0700

    initial

--- backend/digital_twin/connectors/models.py
+++ backend/digital_twin/connectors/models.py
@@ -2,17 +2,15 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Any
 
+from pydantic import BaseModel
 
 from digital_twin.config.constants import DocumentSource
 
-# Sometimes a file could be split into multipe sections due to pagination
-# This class handles that edge case
 @dataclass
 class Section:
-    link: str | None
+    link: str
     text: str
 
-# This is the class that will be partition on the TextSplitter/Chunker
 @dataclass
 class Document:
     id: str  # This must be unique or during indexing/reindexing, chunks will be overwritten
@@ -24,3 +22,24 @@ class Document:
 
 def get_raw_document_text(document: Document) -> str:
     return "\n\n".join([section.text for section in document.sections])
+
+
+class InputType(str, Enum):
+    # e.g. loading a current full state or a save state, such as from a file
+    LOAD_STATE = "load_state"
+    # e.g. calling an API to get all documents in the last hour
+    POLL = "poll"
+    # e.g. registered an endpoint as a listener, and processing connector events
+    EVENT = "event"
+
+
+class ConnectorDescriptor(BaseModel):
+    source: DocumentSource
+    # how the raw data being indexed is procured
+    input_type: InputType
+    # what is passed into the __init__ of the connector described by `source`
+    # and `input_type`
+    connector_specific_config: dict[str, Any]
+
+    class Config:
+        arbitrary_types_allowed = True
