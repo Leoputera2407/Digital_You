adf8369d05047af88bfd5b830c2b7b7f07624494
Leoputera2407 <cookieavarice@gmail.com>
2023-06-19 11:58:06 -0700

    working auth redirect for slack

--- frontend/app/(auth)/slack/interface/page.tsx
+++ frontend/app/(auth)/slack/interface/page.tsx
@@ -1,47 +1,41 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import axios from "axios";
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+"use client"
+import { useSupabase } from '@/lib/auth/authProvider';
+import axios from 'axios';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
+export default function AuthSlack() {
+  const { session } = useSupabase();
+  const router = useRouter();
+  const searchParams = useSearchParams()
+ 
+  const slack_user_id = searchParams.get('slack_user_id') || '';
+  const team_id = searchParams.get('team_id') || '';
 
-export default async function authSlack({
-  searchParams,
-} : {
-  searchParams: { [key: string]: string | undefined };
-}) {
-  const supabase = createServerComponentClient<any>({
-    cookies,
-  })
+  useEffect(() => {
+    async function authenticate() {
+      if (session) {
+        const supabase_user_id = session.user.id;
+        const redirectUri = `${process.env.BACKEND_URL}/slack/server_signup?slack_user_id=${encodeURIComponent(slack_user_id)}&team_id=${encodeURIComponent(team_id)}&supabase_user_id=${encodeURIComponent(supabase_user_id)}`;
 
-  const { data: { session } } = await supabase.auth.getSession();
-  const slack_user_id = searchParams?.slack_user_id || '';
-  const team_id = searchParams?.team_id || '';
+        const response = await axios.get(redirectUri, {
+          headers: {
+            'Authorization': `Bearer ${session.access_token}`,
+          },
+        });
 
-  if (session) {
-    // TODO: Check if already signup, if so, don't redirect 
-
-    const supabase_user_id = session.user.id;
-
-    const redirectUri = `${process.env.BACKEND_URL}/slack/server_signup?slack_user_id=${encodeURIComponent(slack_user_id)}&team_id=${encodeURIComponent(team_id)}&supabase_user_id=${encodeURIComponent(supabase_user_id)}`;
-
-    const response = await axios.get(
-      redirectUri,
-      {
-        headers: {
-          'Authorization': `Bearer ${session.access_token}`,
-        },
+        if (response.status === 201) {
+          router.push('https://slack.com/');
+        } else {
+          console.error(response);
+        }
+      } else {
+        router.push(`/signin?slack_user_id=${encodeURIComponent(slack_user_id)}&team_id=${encodeURIComponent(team_id)}&redirect=${encodeURIComponent(`${process.env.WEB_DOMAIN}/interface/slack`)}`);
       }
-    );
-
-    if (response.status === 201 ) {
-      // TODO: Redirect to Slack App's page after successful authentication
-      redirect('https://slack.com/');
-    } else {
-      console.error(response);
     }
-  } else {
-    // cookie is preserved through the same domain/site
-    redirect(`/signin?slack_user_id=${encodeURIComponent(slack_user_id)}&team_id=${encodeURIComponent(team_id)}&redirect=${encodeURIComponent(`${process.env.WEB_DOMAIN}/interface/slack`)}`);
-  }
-}
 
+    authenticate();
+  }, [session, slack_user_id, team_id, router]);
+
+  return null;
+}
